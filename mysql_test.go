07a8b7ebbe5aa26a8c124cc9aa7f5140cf2692d7
baerwang <1780285721@qq.com>
2021-12-18 17:20:22 +0800

    fix crud

--- mysql_test.go
+++ mysql_test.go
@@ -10,16 +10,22 @@ import (
 
 func TestMysql(t *testing.T) {
 
-	addr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", "root", "root", "127.0.0.1", 3306, "employees")
+	addr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?timeout=1s&readTimeout=1s&writeTimeout=1s&parseTime=true&loc=Local&charset=utf8mb4,utf8", "root", "root", "127.0.0.1", 3306, "employees")
 	db, err := gorm.Open(mysql.Open(addr), &gorm.Config{})
 	if err != nil {
 		t.Fatal("failed to connect database ", err)
 	}
 
-	db.Create(&Employees{EmpNo: 1, FirstName: "wang", LastName: "bear", BirthDate: time.Now(), Gender: "F", HireDate: time.Now()})
+	create := db.Create(&Employees{EmpNo: 1, FirstName: "wang", LastName: "bear", BirthDate: time.Now(), Gender: "F", HireDate: time.Now()})
+	if create.Error != nil {
+		t.Fatal(err)
+	}
 
-	// 读取
 	var emp Employees
-	db.First(&emp, 1)
+	first := db.First(&emp, 1)
+	if first.Error != nil {
+		t.Fatal(err)
+	}
+
 	t.Logf("%+v", emp)
 }
